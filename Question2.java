33e8635da090ce3b5e459252cbcb3482eb3feea6
Singhmayank0217 <coolsinghmayank@gmail.com>
2024-09-13 19:32:43 +0530

    Intersection code in question2

--- Question2.java
+++ Question2.java
@@ -30,3 +30,39 @@ public class Question2 {
         }
     }
 }
+
+
+/*
+// to print the intersection of two array
+import java.util.*;
+public class Intersection {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of first array:");
+        int n = sc.nextInt();
+        int arr1[] = new int[n];
+        
+        System.out.println("Enter the elements of first array:");
+        for (int i = 0; i < n; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        
+        System.out.println("Enter the size of second array:");
+        int m = sc.nextInt();
+        int arr2[] = new int[m];
+        
+        System.out.println("Enter the elements of second array:");
+        for (int i = 0; i < m; i++) {
+            arr2[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr1[i] == arr2[j]) {
+                    System.out.println(arr1[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
+ */
