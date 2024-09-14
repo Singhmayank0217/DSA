b277b0c80f4d375c318842d939805208f1cbb65d
Singhmayank0217 <coolsinghmayank@gmail.com>
2024-09-14 18:07:56 +0530

    Merge Sort logic

---
+++ SortingLogic/MergeSort.java
@@ -0,0 +1,57 @@
+package SortingLogic;
+import java.util.*;
+
+public class MergeSort {
+    public static void mergesort(int arr[], int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int mid = start + (end - start) / 2;
+        mergesort(arr, start, mid);
+        mergesort(arr, mid + 1, end);
+        merge(arr, start, mid, end);
+    }
+
+    public static void merge(int arr[], int start, int mid, int end) {
+        int temp[] = new int[end - start + 1];
+        int i = start;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= end) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        while (j <= end) {
+            temp[k++] = arr[j++];
+        }
+        for (k = 0, i = start; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("Enter the elements of array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        mergesort(arr, 0, n - 1);
+
+        System.out.println("Sorted array is:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
