08ca1addea6e952d8ed2a04c11eb284d71f10f70
Mayank Singh <127119074+Singhmayank0217@users.noreply.github.com>
2024-10-04 12:00:00 +0530

    hasing Basics 

---
+++ Hashing/basics.java
@@ -0,0 +1,30 @@
+package Hashing;
+
+import java.util.LinkedList;
+
+public class basics {
+    static class HashMap<k, v> {
+        private class Node {
+            k Key;
+            v Value;
+
+            public Node(k Key, v Value) {
+                this.key = Key;
+                this.value = value;
+            }
+        }
+
+        private int n;
+        private int N;
+        private LinkedList<Node> buckets[];
+
+    }
+
+}
+// put() - is used to add th data
+// get() - is used to get the data
+// remove() - is used to remove the data
+// containsKey() - is used to check if the key is present or not
+// containsValue() - is used to check if the value is present or not
+// clear() - is used to clear the data
+// keySet - is used to get the enitre data of the key
