ac3a48e87f99b7d2c9c8b5639602637961b2fa73
Mayank Singh <127119074+Singhmayank0217@users.noreply.github.com>
2024-10-07 12:00:00 +0530

    Next Permutation  

--- Array/occuranceCount.java
+++ Array/occuranceCount.java
@@ -20,3 +20,21 @@ public class occuranceCount {
         }
     }
 }
+
+// using hashing
+/*
+ * class Solution {
+ * public int majorityElement(int[] nums) {
+ * Map<Integer, Integer> counts = new HashMap<>();
+ * int n = nums.length;
+ * 
+ * for (int num : nums) {
+ * counts.put(num, counts.getOrDefault(num, 0) + 1);
+ * if (counts.get(num) > n / 2) {
+ * return num;
+ * }
+ * }
+ * return -1;
+ * }
+ * }
+ */
