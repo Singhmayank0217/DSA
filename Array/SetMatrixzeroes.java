aee171105121d7768288f908783a2260d955282d
Mayank Singh <127119074+Singhmayank0217@users.noreply.github.com>
2024-10-09 12:00:00 +0530

    matrix optimal solution  

---
+++ Array/SetMatrixzeroes.java
@@ -0,0 +1,41 @@
+package Array;
+
+public class SetMatrixzeroes {
+
+    public void setZeroes(int[][] arr) {
+        int n = arr.length;
+        int m = arr[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 0) {
+                    markRow(arr, i, m);
+                    markCol(arr, j, n);
+                }
+            }
+        }
+        // Second pass to replace -1 with 0
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == -1) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    private void markRow(int[][] arr, int i, int m) {
+        for (int j = 0; j < m; j++) {
+            if (arr[i][j] != 0) {
+                arr[i][j] = -1;
+            }
+        }
+    }
+
+    private void markCol(int[][] arr, int j, int n) {
+        for (int i = 0; i < n; i++) {
+            if (arr[i][j] != 0) {
+                arr[i][j] = -1;
+            }
+        }
+    }
+}
