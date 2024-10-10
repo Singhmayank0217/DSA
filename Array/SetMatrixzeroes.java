0c1dc41880e50e04592cbe67f3b81731462909f0
Mayank Singh <127119074+Singhmayank0217@users.noreply.github.com>
2024-10-10 12:00:00 +0530

    Rotate Image brute force  

--- Array/SetMatrixzeroes.java
+++ Array/SetMatrixzeroes.java
@@ -39,3 +39,59 @@ public class SetMatrixzeroes {
         }
     }
 }
+
+// another ffective technique to solve the question
+/*
+ * class Solution {
+ * public void setZeroes(int[][] arr) {
+ * int m = arr.length;
+ * int n = arr[0].length;
+ * boolean firstRowZero = false;
+ * boolean firstColZero = false;
+ * for (int j = 0; j < n; j++) {
+ * if (arr[0][j] == 0) {
+ * firstRowZero = true;
+ * break;
+ * }
+ * }
+ * for (int i = 0; i < m; i++) {
+ * if (arr[i][0] == 0) {
+ * firstColZero = true;
+ * break;
+ * }
+ * }
+ * for (int i = 1; i < m; i++) {
+ * for (int j = 1; j < n; j++) {
+ * if (arr[i][j] == 0) {
+ * arr[i][0] = 0;
+ * arr[0][j] = 0;
+ * }
+ * }
+ * }
+ * for (int i = 1; i < m; i++) {
+ * if (arr[i][0] == 0) {
+ * for (int j = 1; j < n; j++) {
+ * arr[i][j] = 0;
+ * }
+ * }
+ * }
+ * for (int j = 1; j < n; j++) {
+ * if (arr[0][j] == 0) {
+ * for (int i = 1; i < m; i++) {
+ * arr[i][j] = 0;
+ * }
+ * }
+ * }
+ * if (firstRowZero) {
+ * for (int j = 0; j < n; j++) {
+ * arr[0][j] = 0;
+ * }
+ * }
+ * if (firstColZero) {
+ * for (int i = 0; i < m; i++) {
+ * arr[i][0] = 0;
+ * }
+ * }
+ * }
+ * }
+ */
