327668875bba7013445e4b45ce61fc49f0f53510
Mayank Singh <127119074+Singhmayank0217@users.noreply.github.com>
2025-01-02 12:00:00 +0530

    spiral matrix solution  

--- Array/rotateImage.java
+++ Array/rotateImage.java
@@ -18,3 +18,35 @@ public class rotateImage {
         }
     }
 }
+
+// optimal solution :
+/*
+ * class Solution {
+ * public void rotate(int[][] arr) {
+ * int n = arr.length;
+ * // Transpose the matrix
+ * for (int i = 0; i < n; i++) {
+ * for (int j = i + 1; j < n; j++) {
+ * swap(arr, i, j, j, i);
+ * }
+ * }
+ * // Reverse each row
+ * for (int i = 0; i < n; i++) {
+ * reverseRow(arr, i);
+ * }
+ * }
+ * 
+ * private void swap(int[][] arr, int i1, int j1, int i2, int j2) {
+ * int temp = arr[i1][j1];
+ * arr[i1][j1] = arr[i2][j2];
+ * arr[i2][j2] = temp;
+ * }
+ * 
+ * private void reverseRow(int[][] arr, int row) {
+ * int n = arr.length;
+ * for (int i = 0; i < n / 2; i++) {
+ * swap(arr, row, i, row, n - 1 - i);
+ * }
+ * }
+ * }
+ */
