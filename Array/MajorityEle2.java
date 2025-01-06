79f556b6f642eff662295e42addf9662d529891c
Mayank Singh <127119074+Singhmayank0217@users.noreply.github.com>
2025-01-06 12:00:00 +0530

    Majority element better solution  

--- Array/MajorityEle2.java
+++ Array/MajorityEle2.java
@@ -24,5 +24,25 @@ public class MajorityEle2 {
         }
         return res;
     }
-
 }
+
+// Better solution
+
+/*
+ * class Solution {
+ * public List<Integer> majorityElement(int[] nums) {
+ * Map<Integer, Integer> counts = new HashMap<>();
+ * int n = nums.length;
+ * for (int num : nums) {
+ * counts.put(num, counts.getOrDefault(num, 0) + 1);
+ * }
+ * List<Integer> result = new ArrayList<>();
+ * for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+ * if (entry.getValue() > n / 3) {
+ * result.add(entry.getKey());
+ * }
+ * }
+ * return result;
+ * }
+ * }
+ */
