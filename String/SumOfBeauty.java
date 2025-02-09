dc2668483f71fde136e2ab1526c1826caf503dc5
Mayank Singh <127119074+Singhmayank0217@users.noreply.github.com>
2025-02-09 12:00:00 +0530

    Sum of beauty using string

--- String/SumOfBeauty.java
+++ String/SumOfBeauty.java
@@ -24,3 +24,28 @@ public class SumOfBeauty {
         }
     }
 }
+
+/*
+ * using string
+ * class Solution {
+ * public int beautySum(String s) {
+ * int totalBeauty = 0;
+ * int n = s.length();
+ * for (int i = 0; i < n; i++) {
+ * int[] freq = new int[26];
+ * for (int j = i; j < n; j++) {
+ * freq[s.charAt(j) - 'a']++;
+ * int maxFreq = 0, minFreq = Integer.MAX_VALUE;
+ * for (int f : freq) {
+ * if (f > 0) {
+ * maxFreq = Math.max(maxFreq, f);
+ * minFreq = Math.min(minFreq, f);
+ * }
+ * }
+ * totalBeauty += (maxFreq - minFreq);
+ * }
+ * }
+ * return totalBeauty;
+ * }
+ * }
+ */
