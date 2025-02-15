0896654f894dca405b2b423f469eb89868427870
Mayank Singh <127119074+Singhmayank0217@users.noreply.github.com>
2025-02-15 12:00:00 +0530

    Linked List Insertion At End

--- LinkedList/ListAtEnd.java
+++ LinkedList/ListAtEnd.java
@@ -1,7 +1,20 @@
 package LinkedList;
 
 public class ListAtEnd {
-    Linked List
-    Insertion At End
+    class Solution {
+        // Function to insert a node at the end of the linked list.
+        Node insertAtEnd(Node head, int x) {
+            Node newNode = new Node(x);
+            if (head == null) {
+                return newNode;
+            }
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+            return head;
+        }
+    }
 
 }
