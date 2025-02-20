33da8d55efa0cb45145e3e56e6653487e65611c6
Mayank Singh <127119074+Singhmayank0217@users.noreply.github.com>
2025-02-20 12:00:00 +0530

    return the key if present

---
+++ LinkedList/CountNodes.java
@@ -0,0 +1,16 @@
+package LinkedList;
+
+public class CountNodes {
+    class Solution {
+        // Function to count nodes of a linked list.
+        public int getCount(Node head) {
+            int count = 0;
+            Node temp = head;
+            while (temp != null) {
+                count++;
+                temp = temp.next;
+            }
+            return count;
+        }
+    }
+}
