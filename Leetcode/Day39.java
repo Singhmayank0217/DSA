3f1bd52d4350608820bfe442fa2f0a22d38965f0
Mayank Singh <127119074+Singhmayank0217@users.noreply.github.com>
2025-03-12 21:06:32 +0530

    day 39

---
+++ Leetcode/Day39.java
@@ -0,0 +1,17 @@
+public class Day39 {
+    class Solution {
+        public int maximumCount(int[] nums) {
+            int negCount = 0;
+            int posCount = 0;
+            for (int num : nums) {
+                if (num < 0) {
+                    negCount++;
+                } else if (num > 0) {
+                    posCount++;
+                }
+            }
+            return Math.max(negCount, posCount);
+        }
+    }
+
+}
